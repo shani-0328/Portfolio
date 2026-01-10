5efac846a6dd52d5be72c8c0eacfaf1ae81b4574
RandikaMa <wimalawardanar@gmail.com>
2026-01-10 15:50:36 +0530

    Fix resume link in Hero component to use RetroButton with basePath

--- frontend/app/components/HeroRetroModernEnhanced.tsx
+++ frontend/app/components/HeroRetroModernEnhanced.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect, useRef } from 'react';
 import { frontendPortfolioData } from '../utils/frontendData';
 import { getAssetPath } from '../utils/assetPath';
 import { scrollToSection } from '../utils/scrollToSection';
+import RetroButton from './RetroButton';
 
 interface Portfolio {
   name: string;
@@ -143,15 +144,15 @@ export default function HeroRetroModernEnhanced() {
                 <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
               </svg>
               View Projects
-            </button>            <a
+            </button>            <RetroButton
+              variant="outline"
               href="/resume"
-              className="font-[family-name:var(--font-orbitron)] px-8 py-3 bg-gradient-to-r from-amber-400/20 to-amber-500/20 text-yellow-300 border-2 border-yellow-400/70 rounded-md hover:bg-yellow-500/20 transition-all duration-300 flex items-center shadow-lg shadow-amber-900/20"
             >
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z" clipRule="evenodd" />
               </svg>
               Resume
-            </a>
+            </RetroButton>
           </div>
           {/* Enhanced social icons */}
           <div className="mt-8 flex space-x-6">
